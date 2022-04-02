88e678333c502ab4c30a5a75442d9918518bfb43
K4speeer <dri.taha24@gmail.com>
2022-04-03 00:55:29 +0300

    uploading the changes

--- Simon Game/Simon Game Challenge Starting Files/game.js
+++ Simon Game/Simon Game Challenge Starting Files/game.js
@@ -1,13 +1,34 @@
-var buttonColors = ["red", "blue", "green", "yellow"]; 
+var buttonColors = ["red", "green", "blue", "yellow"];
+var buttonSounds = ["./sounds/red.mp3", "./sounds/green.mp3", "./sounds/blue.mp3", "./sounds/yellow.mp3"];
 
 var gamePattern = [];
-
-
+var playersPattern = [];
 
 function nextSequance (){
   var randomNumber = Math.floor(Math.random()*4);
-  return randomNumber;
-}
+  var choosenColor = buttonColors[randomNumber];
+  gamePattern.push(choosenColor);
+  $("#"+choosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+  var audio = new Audio (buttonSounds[randomNumber]);
+  audio.play();
+};
 
-var randomChosenColor = buttonColors[nextSequance];
-gamePattern.push(randomChosenColor);
+function playSounds (userChoosenColor){
+  var audio = new Audio ("./sounds/" + userChoosenColor + ".mp3");
+  audio.play();
+};
+
+function flashing (userChoosenColor){
+  $("#"+userChoosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+};
+
+
+$(".btn").click(function(){
+  var userChoosenColor = this.getAttribute("id");
+  playSounds(userChoosenColor);
+  flashing(userChoosenColor);
+  playersPattern.push(userChoosenColor);
+});
+
+
+
