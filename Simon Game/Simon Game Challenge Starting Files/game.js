e3ebbec45d9bf7ee861c6dec6017f75c2807883a
K4speeer <dri.taha24@gmail.com>
2022-04-10 00:06:56 +0300

    Updating the soluthion (Step 7)

--- Simon Game/Simon Game Challenge Starting Files/game.js
+++ Simon Game/Simon Game Challenge Starting Files/game.js
@@ -1,16 +1,25 @@
 var buttonColors = ["red", "green", "blue", "yellow"];
 var buttonSounds = ["./sounds/red.mp3", "./sounds/green.mp3", "./sounds/blue.mp3", "./sounds/yellow.mp3"];
 
-var gamePattern = [];
+var gamePattern = []; 
 var playersPattern = [];
+var level = 0;
+
+var started = false;
 
 function nextSequance (){
+
+  level++;
+  $("#level-title").html("Level " + level);
+
   var randomNumber = Math.floor(Math.random()*4);
   var choosenColor = buttonColors[randomNumber];
   gamePattern.push(choosenColor);
+
   $("#"+choosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
   var audio = new Audio (buttonSounds[randomNumber]);
   audio.play();
+  
 };
 
 function playSounds (userChoosenColor){
@@ -18,17 +27,34 @@ function playSounds (userChoosenColor){
   audio.play();
 };
 
-function flashing (userChoosenColor){
-  $("#"+userChoosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+// function flashing (userChoosenColor){
+//   $("#"+userChoosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+// };
+function animatePress (currentColor){
+  $("#" + userChoosenColor).addClass("pressed");
+  setTimeout(function(){
+    $("#" + userChoosenColor).removeClass("pressed");
+  },100);
 };
 
-
 $(".btn").click(function(){
+
   var userChoosenColor = this.getAttribute("id");
-  playSounds(userChoosenColor);
-  flashing(userChoosenColor);
   playersPattern.push(userChoosenColor);
+
+  animatePress(userChoosenColor);
+  playSounds(userChoosenColor);
+  // flashing(userChoosenColor);
+
 });
 
+$(document).keypress(function(){
+  if(!started){
 
+    $("level-title").text("level " + level);
+    nextSequance();
+    started = true;
+  }
+  
+});
 
